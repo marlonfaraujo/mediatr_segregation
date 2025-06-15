fb1c030cfbc1e29e60b1fc9cffd5875a9f1bbbb0
Marlon <marlonfda@gmail.com>
2025-06-15 20:09:44 -0300

    Adding my mediator to the project, taking advantage of the abstraction of being able to choose framework options

---
+++ src/MediartSegregation.Shared/MyMediator/MyMediatorNotificationAdapter.cs
@@ -0,0 +1,21 @@
+﻿using MediartSegregation.Domain.Shared.Abstractions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediartSegregation.Shared.MyMediator
+{
+    public class MyMediatorNotificationAdapter : IDomainNotificationAdapter 
+    {
+        private readonly MyMediator _mediator;
+
+        public MyMediatorNotificationAdapter(MyMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        public async Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken) where TNotification : BaseDomainEvent
+        {
+            await _mediator.PublishAsync<TNotification>(notification, cancellationToken);
+        }
+    }
+}
--- src/MediatRSegregation.Api/Program.cs
+++ src/MediatRSegregation.Api/Program.cs
@@ -1,6 +1,7 @@
 using MediartSegregation.Application;
 using MediartSegregation.Application.Ping.UseCases.CreatePing;
 using MediartSegregation.Application.UseCases.Ping.GetPing;
+using MediartSegregation.Shared.MyMediator;
 using MediatR;
 using MediatRSegregation.Shared.MediatRExtensions;
 using MediatRSegregation.Shared.MediatRExtensions.Implementation;
@@ -15,6 +16,7 @@ builder.Services.AddMediatR(cfg =>
     );
 });
 builder.Services.AddAdapterRegistrationExtensions(typeof(Application).Assembly);
+builder.Services.AddScoped<MyMediator>();
 
 var app = builder.Build();
 
@@ -27,5 +29,23 @@ app.MapPost("/ping", async () =>
     var getPingResponse = await mediator.Send(new MediatRRequestAdapter<GetPingQuery, GetPingResponse>(new GetPingQuery()));
     return new { CreateMessage = createPingResponse.Message, GetMessage = getPingResponse.Message };
 });
+
+
+app.MapGet("/pong", async () =>
+{
+    var myMediator = serviceProvider.GetRequiredService<MyMediator>();
+    var myMediatorNotification = new MyMediatorNotificationAdapter(myMediator);
+
+    var createPingResponse = await myMediator.SendAsync<CreatePingCommand,CreatePingResponse>(
+        new CreatePingCommand("pong"), 
+        new CreatePingHandler(myMediatorNotification));
+
+    var getPingResponse = await myMediator.SendAsync<GetPingQuery, GetPingResponse>(
+        new GetPingQuery("pong"), 
+        new GetPingHandler(myMediatorNotification));
+
+    return new { CreateMessage = createPingResponse.Message, GetMessage = getPingResponse.Message };
+});
+
 
 app.Run();
